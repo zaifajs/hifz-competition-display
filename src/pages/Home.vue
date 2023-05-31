bb384c26c2619d1b4e377dfb7e9e588054cc310e
Nabil <nabil_code@outlook.com>
2023-06-01 01:45:54 +0600

    refactor App.vue to use vue router
    Signed-off-by: Nabil <nabil_code@outlook.com>

---
+++ src/pages/Home.vue
@@ -0,0 +1,61 @@
+<script setup>
+const { selectedProfile } = defineProps({
+  selectedProfile: Object | null,
+});
+</script>
+
+<template>
+  <main v-if="selectedProfile" class="">
+    <div class="main-content container">
+      <div class="left-panel">
+        <img :src="'src/assets/images/participants/' + selectedProfile['PARTICIPANT_PHOTO']" />
+      </div>
+
+      <div class="right-panel">
+
+        <div class="name-container">
+          <div class="name">
+            {{ selectedProfile['FIRST_NAME'] }}
+            <span class="name-last">{{ selectedProfile['LAST_NAME'] }}</span>
+          </div>
+        </div>
+        <div class="details-container">
+          <div class="div">
+            <div class="age-container">
+              <div class="meta">
+                <div class="title">AGE</div>
+                <div class="sub-title">AGE</div>
+              </div>
+              <span class="age">{{ selectedProfile['AGE_ON_EVENT'] }}</span>
+            </div>
+            <div class="country">
+              <div class="flag">
+                <div class="meta">
+                  <div class="title">COUNTRY</div>
+                  <div class="sub-title">PAÍS</div>
+                </div>
+                <div class="flag-detials">
+                  <img width="100" :src="'flags/' + selectedProfile['FLAG_IMAGE']" class="flag" />
+                  <span>{{ selectedProfile['FLAG'] }}</span>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="category">
+            <div class="meta">
+              <div class="title">CATEGORY</div>
+              <div class="sub-title">CATEGORIA</div>
+            </div>
+            <img :src="'categories/' + selectedProfile['CATEGORY_IMAGE']" />
+          </div>
+        </div>
+        <div class="logo-container">
+          <img height="85" src="../assets/images/organization_logos.jpg" />
+        </div>
+      </div>
+
+    </div>
+  </main>
+</template>
+
+<style></style>
---
+++ src/pages/TvOutput.vue
@@ -0,0 +1,35 @@
+<script setup>
+const { selectedProfile } = defineProps({
+  selectedProfile: Object | null,
+});
+</script>
+
+
+<template>
+  <main v-if="selectedProfile" class="">
+    <div class="main-content container">
+
+      <div class="left-panel">
+      </div>
+
+      <div class="right-panel">
+        <div class="name-container">
+
+        </div>
+        <div class="details-container">
+          {{ selectedProfile['FIRST_AND_LAST_NAME'] }}
+          <div class="flag-detials">
+            <img width="100" :src="'flags/' + selectedProfile['FLAG_IMAGE']" class="flag" />
+            <span>{{ selectedProfile['FLAG'] }}</span>
+          </div>
+        </div>
+
+        <div class="logo-container">
+          <img height="85" src="../assets/images/organization_logos.jpg" />
+
+          <img :src="'categories/' + selectedProfile['CATEGORY_IMAGE']" />
+        </div>
+      </div>
+    </div>
+  </main>
+</template>
