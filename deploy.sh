79024474fd55b2b747731fd2a29a9b24995702ed
Nabil <nabil_code@outlook.com>
2023-06-02 15:36:54 +0600

    add deploy script
    Signed-off-by: Nabil <nabil_code@outlook.com>

---
+++ deploy.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env sh
+
+# abort on errors
+set -e
+
+npm run build
+
+cd dist
+
+git init
+git add -A
+git commit -m 'deploy'
+
+git push -f git@github.com:fipalmela/hifz-competition.git master:gh-pages
+
+cd -
