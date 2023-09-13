8d3f743f066ffc34e615195112a88ddf2b7ba021
zhuzaifa <huzaifa@brixel.io>
2023-09-13 18:52:42 +0100

    fix gen

--- src/router.js
+++ src/router.js
@@ -7,14 +7,9 @@ import TvOutput from './pages/TvOutput.vue'
 const routes = [
   { path: ROUTE_HOME, component: Home, props: true },
   { path: ROUTE_TV_OUTPUT, component: TvOutput, props: true },
-  {
-    path: '/generator',
-    beforeEnter() { location.href = '/fip/generator/index.html' }
-  }
 ]
 
-
 export default createRouter({
-  history: createWebHistory('/fip/'),
+  history: createWebHistory(),
   routes,
 });
